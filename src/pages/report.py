4ab79853d40035338ee8667a3b4aa2d3f9698cb9
lyeka <augustr2017@163.com>
2025-10-16 14:29:30 +0800

    feat: enhance report page with tabbed layout for single stock and comparison analysis

--- src/pages/report.py
+++ src/pages/report.py
@@ -1,6 +1,14 @@
 import streamlit as st
 from streamlit_file_browser import st_file_browser
 
-st.header('Analysis Report')
-event = st_file_browser("report", key='AnalyzeReport',extentions=['.md'])
-st.write(event)
+
+tab1, tab2 = st.tabs(["Single Stock", "Compare Stock"])
+
+with tab1:
+    st.header('Analysis Report')
+    event = st_file_browser("report/single_stock", key='SingleStockReport',extentions=['.md'])
+    st.write(event)
+with tab2:
+    st.header('Analysis Report')
+    event = st_file_browser("report/compare_stock", key='CompareStockReport',extentions=['.md'])
+    st.write(event)
